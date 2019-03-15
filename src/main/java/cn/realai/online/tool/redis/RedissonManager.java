394daa1818b389cd18ec130a892a6a6186b0154f
86183 <86183@192.168.0.101>
2019-03-15 23:03:13 +0800

    测试提交

--- src/main/java/cn/realai/online/tool/redis/RedissonManager.java
+++ src/main/java/cn/realai/online/tool/redis/RedissonManager.java
@@ -36,6 +36,7 @@ public class RedissonManager {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+        System.out.println("qwe");
         //可通过打印redisson.getConfig().toJSON().toString()来检测是否配置成功
         return redisson;
     }
