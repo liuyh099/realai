a5f78c9aee9eb56529fa89faf0e3af6e06aa228b
1196837833@qq.com <kaerbaqian321>
2019-03-20 17:07:43 +0800

    #获取变量数据列表修改#

--- src/main/java/cn/realai/online/core/service/VariableDataService.java
+++ src/main/java/cn/realai/online/core/service/VariableDataService.java
@@ -2,6 +2,7 @@ package cn.realai.online.core.service;
 
 import java.util.List;
 
+import cn.realai.online.core.bo.VariableDataBO;
 import cn.realai.online.core.entity.VariableData;
 
 public interface VariableDataService {
