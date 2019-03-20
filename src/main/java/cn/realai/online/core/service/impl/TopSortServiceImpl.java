1bb8a76aa35bdd9c2c8226752b6282656bc1f7e3
Liuyuhan <yuhan.liu@realai.ai>
2019-03-20 17:51:07 +0800

    训练回调处理

--- src/main/java/cn/realai/online/core/service/impl/TopSortServiceImpl.java
+++ src/main/java/cn/realai/online/core/service/impl/TopSortServiceImpl.java
@@ -2,17 +2,28 @@ package cn.realai.online.core.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import cn.realai.online.core.dao.TopSortDao;
 import cn.realai.online.core.entity.TopSort;
 import cn.realai.online.core.service.TopSortService;
 
 @Service
 public class TopSortServiceImpl implements TopSortService {
 
+	@Autowired
+	private TopSortDao topSortDao;
+	
 	@Override
 	public void insertList(List<TopSort> tsList) {
-		
+		if (tsList == null || tsList.size() == 0) {
+			return;
+		}
+		for (TopSort ts : tsList) {
+			ts.setCreateTime(System.currentTimeMillis());
+		}
+		topSortDao.insertList(tsList);
 	}
 
 }
