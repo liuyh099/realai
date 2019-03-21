789bd7770560749cad6db2d1c3455bd19f8fcd86
0nlyl <996463990@qq.com>
2019-03-21 13:35:18 +0800

    fixes #查询用户列表调整

--- src/main/java/cn/realai/online/userandperm/query/UserPageQuery.java
+++ src/main/java/cn/realai/online/userandperm/query/UserPageQuery.java
@@ -7,7 +7,7 @@ import io.swagger.annotations.ApiModelProperty;
 @ApiModel
 public class UserPageQuery extends PageQuery {
 
-    @ApiModelProperty(value = "是否是查询忘记密码的", example = "1:表示是查询忘记密码的用户 其他为所有用户")
+    @ApiModelProperty(value = "是否是查询忘记密码的 1:表示是查询忘记密码的用户 其他或者不传为所有用户", example = "1:表示是查询忘记密码的用户 其他为所有用户")
     private Integer forget;
 
     public Integer getForget() {
