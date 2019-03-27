8bdea6c9be004c3b7575f8f1c8098b8871c8ccb2
0nlyl <996463990@qq.com>
2019-03-27 16:06:58 +0800

    fixes # 定期清除180秒未活跃的session,新建实验未保存服务ID修改

--- src/main/java/cn/realai/online/common/config/MySessionManager.java
+++ src/main/java/cn/realai/online/common/config/MySessionManager.java
@@ -1,15 +1,33 @@
 package cn.realai.online.common.config;
 
+import cn.realai.online.util.SpringContextUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.servlet.ShiroHttpServletRequest;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.apache.shiro.web.util.WebUtils;
+import org.crazycake.shiro.RedisCacheManager;
+import org.crazycake.shiro.exception.SerializationException;
+import org.crazycake.shiro.serializer.RedisSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
 
 public class MySessionManager extends DefaultWebSessionManager {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private static String a ="cn.realai.online.common.config.MyShiroRealm.authorizationCache:";
+
+    RedisCacheManager redisCacheManager;
 
     private static final String AUTHORIZATION = "mySessionId";
 
@@ -30,6 +48,42 @@ public class MySessionManager extends DefaultWebSessionManager {
         } else {
             //否则按默认规则从cookie取sessionId
             return super.getSessionId(request, response);
+        }
+    }
+
+    @Override
+    public void validateSessions() {
+        super.validateSessions();
+        Collection<Session> sessions = getActiveSessions();
+        if (!CollectionUtils.isEmpty(sessions)) {
+            if(redisCacheManager==null){
+                synchronized (a){
+                    redisCacheManager= (RedisCacheManager) SpringContextUtils.getBean("myRedisCacheManager");
+                }
+            }
+            for (Session session : sessions) {
+                Date date = session.getLastAccessTime();
+                long last = date.getTime();
+                long current = System.currentTimeMillis();
+                //if(current-last>180000){
+                if (current - last > 180000) {
+                    Object o =session.getAttribute("userId");
+                    if(o!=null){
+                        String userId = (String)o ;
+
+                        String preFix = redisCacheManager.getKeyPrefix();
+                        RedisSerializer redisSerializer=redisCacheManager.getKeySerializer();
+                        try {
+                            byte[] key = redisSerializer.serialize(preFix+a+userId);
+                            redisCacheManager.getRedisManager().del(key);
+                        } catch (SerializationException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    super.delete(session);
+                }
+            }
+
         }
     }
 }
