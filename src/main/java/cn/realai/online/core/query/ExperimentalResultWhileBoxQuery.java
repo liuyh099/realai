21b27a5eb8d6ff94cb4c123f5130ff5b0186e955
Liuyuhan <yuhan.liu@realai.ai>
2019-04-03 12:28:15 +0800

    提交

--- src/main/java/cn/realai/online/core/query/ExperimentalResultWhileBoxQuery.java
+++ src/main/java/cn/realai/online/core/query/ExperimentalResultWhileBoxQuery.java
@@ -1,5 +1,7 @@
 package cn.realai.online.core.query;
 
+import javax.validation.constraints.NotNull;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
@@ -9,11 +11,22 @@ public class ExperimentalResultWhileBoxQuery extends PageQuery {
     @ApiModelProperty(value = "实验ID")
     private Long experimentId;
     @ApiModelProperty(value = "分组ID")
+    @NotNull
     private Long groupId;
     @ApiModelProperty(value = "变量类型 2:同质 1:异质 为空表示查询所有", example = "2:同质 1:异质 为空表示查询所有")
     private Integer variableType;
     @ApiModelProperty(value = "请求类型 image:请求图片 normal:普通请求", example = "image:请求图片 normal:普通请求")
     private String type;
+
+    private String searchType;
+
+    public String getSearchType() {
+        return searchType;
+    }
+
+    public void setSearchType(String searchType) {
+        this.searchType = searchType;
+    }
 
     public Long getExperimentId() {
         return experimentId;
